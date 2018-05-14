562e649cbcff03e2a2ddf81b77355c3a589f3e01
ValerianUrbanski <37069513+ValerianUrbanski@users.noreply.github.com>
2018-05-14 16:39:54 +0200

    lot of improvement
    using canvas instead of GridPane also implementing game loop for frame performance and other many keybord shortcut and user interface

--- src/fr/autre/jeudelavie/ihm/Frame.java
+++ src/fr/autre/jeudelavie/ihm/Frame.java
@@ -2,143 +2,260 @@ package fr.autre.jeudelavie.ihm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Frame extends Application{
-	private int size = 100;
-	private CustomPane[][] lstPane = new CustomPane[size][size];
+	private int size = 700;
+	private int scale = 10;
+	private Cells[][] lstPane = new Cells[size][size];
+	private List<Cells> lstCells = new ArrayList<Cells>();
 	private boolean running = true;
 	private boolean paused = false;
+	private long start;
+	int frameCnt;
+	private double gameSpeedSecs = 0.01667;
+	private Loop loop;
+	private int targetFrame = 60;
 
+	private Label targetFrameLabel = new Label();
+	private Label actualFrameLabel = new Label();
+	private Label currentStep = new Label();
+
+	private int translatedX=0;
+	private int translatedY=0;
+
+	private boolean step = false;
+	private boolean moveTo = false;
+
+	private int stepNumber = 0;
+	private int moveToNumber = 0;
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage = new Stage();
-
-		GridPane gridPane = new GridPane();
-		gridPane.setVgap(1);
-		gridPane.setHgap(1);
-		gridPane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-		for(int i =0;i<size;i++){
-			for(int y=0;y<size;y++){
-				CustomPane pane = new CustomPane();
-				pane.setMinSize(10, 10);
-				pane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-				pane.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-				pane.setOnMouseClicked(e->{
-					if(pane.isAlive()){
-						pane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-						pane.setAlive(false);
+		start = System.currentTimeMillis();
+		BorderPane pane = new BorderPane();
+		Scene scene = new Scene(pane,700,700, Color.BLACK);
+		Canvas canvas = new Canvas((size*scale)+size, (size*scale)+size);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.WHITE);
+		canvas.setOnMouseClicked(e->{
+			int x = (int) e.getX();
+			int y = (int) e.getY();
+			x = x-translatedX;
+			y=y-translatedY;
+			System.out.println(x+" "+y);
+			for(Cells inLstCells : lstCells){
+				if(x-inLstCells.getCoordX() >= 0 && x-inLstCells.getCoordX()<= scale && y-inLstCells.getCoordY()>=0 && y-inLstCells.getCoordY()<=scale){
+					if(inLstCells.isAlive()){
+						gc.setFill(Color.BLACK);
+						gc.fillRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+						inLstCells.setAlive(false);
 					}
 					else{
-						pane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-						pane.setAlive(true);
-					}
-				});
-				lstPane[i][y] = pane;
-				gridPane.add(pane, i, y);
-			}
-		}
-		new Thread(){
-			public void run() {
-				while(true && running){
-					List<CustomPane> alive = new ArrayList<CustomPane>();
-					List<CustomPane> toRez = new ArrayList<CustomPane>();
-					System.out.println("doshit");
-					if(!paused){
-						for(int i=0;i<size;i++){
-							for(int y=0;y<size;y++){
-								int nbAlive = 0;
-								int difx =0;
-								if(i>0){
-									if(i<size-1){
-										difx=1;
-									}
-									else{
-										difx=0;
-									}
-								}
-								else{
-									difx=0;
-								}
-								int dify=0;
-								if(y>0){
-									if(y<size-1){
-										dify=1;
-									}
-									else{
-										dify=0;
-									}
-								}
-								else{
-									dify=0;
-								}
-								if(lstPane[i-difx][y-dify].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i][y-dify].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i+difx][y-dify].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i-difx][y].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i+difx][y].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i-difx][y+dify].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i][y+dify].isAlive()){
-									nbAlive++;
-								}
-								if(lstPane[i+difx][y+dify].isAlive()){
-									nbAlive++;
-								}
-								if(nbAlive>3 || nbAlive<2){
-									alive.add(lstPane[i][y]);
-								}
-								else if(nbAlive==3){
-									toRez.add(lstPane[i][y]);
-								}
-							}
-						}
-						for(CustomPane inAlive : alive){
-							if(inAlive.isAlive()){
-								inAlive.setAlive(false);
-								inAlive.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-							}
-						}
-						alive.clear();
-						for(CustomPane inToRez : toRez){
-							if(!inToRez.isAlive()){
-								inToRez.setAlive(true);
-								inToRez.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-							}
-						}
-						toRez.clear();
+						gc.setFill(Color.WHITE);
+						gc.fillRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+						inLstCells.setAlive(true);
 					}
 				}
 			}
-		}.start();
-		Scene scene = new Scene(gridPane,700,700);
+		});
+		Pane panel = new Pane();
+
+		Player player = new Player(0,0);
+		panel.setTranslateX(player.getX());
+		panel.setTranslateY(player.getY());
+		panel.getChildren().add(canvas);
+		pane.setCenter(panel);
+		VBox box = new VBox();
+		TextField field = new TextField();
+		Button stepBtn = new Button("Step");
+		Button resetStep = new Button("Reset Step");
+		Button moveToBtn = new Button("Move to Step");
+		resetStep.setOnAction(e->{
+			stepNumber = 0;
+		});
+		stepBtn.setOnAction(e->{
+			step = true;
+		});
+		moveToBtn.setOnAction(e->{
+			try{
+				moveToNumber=Integer.valueOf(field.getText());
+				moveTo = true;
+				field.setText("");
+				stepNumber = 0;
+			}
+			catch(Exception e1){
+				e1.printStackTrace();
+			}
+		});
+		Slider slider = new Slider();
+		slider.setDisable(true);
+		box.setOnMouseEntered(e->{
+			slider.setDisable(false);
+		});
+		box.setOnMouseExited(e->{
+			slider.setDisable(true);
+		});
+		slider.setOnMouseEntered(e->{
+			slider.setDisable(false);
+		});
+		slider.setOnMouseExited(e->{
+			slider.setDisable(true);
+		});
+		slider.setMax(120);
+		slider.setValue(targetFrame);
+		slider.setOnMouseReleased(e->{
+			if(Math.round(slider.getValue()) == 0){
+				slider.setValue(1);
+			}
+			slider.setValue(Math.round(slider.getValue()));
+			targetFrameLabel.setText("Target Frame : "+String.valueOf(slider.getValue()));
+			setTargetFrame((int) slider.getValue());
+			gameSpeedSecs = 1/slider.getValue();
+			loop.setUpdate((long)(gameSpeedSecs * 1000000000L));
+		});
+		box.getChildren().addAll(targetFrameLabel,actualFrameLabel,slider,stepBtn,currentStep,resetStep,field,moveToBtn);
+		pane.setLeft(box);
+		pane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+		targetFrameLabel.setText("Target Frame : "+String.valueOf(getTargetFrame()));
+		targetFrameLabel.setTextFill(Color.WHITE);
+		for(int i =0;i<size;i++){
+			for(int y=0;y<size;y++){
+				Cells cell = new Cells((i*scale)+i,(y*scale)+y);
+				lstPane[i][y] = cell;
+				lstCells.add(cell);
+			}
+		}
+		loop = new Loop((long)(gameSpeedSecs * 1000000000L), elapsed->{
+			List<Cells> alive = new ArrayList<Cells>();
+			List<Cells> toRez = new ArrayList<Cells>();
+			if(stepNumber == moveToNumber){
+				moveTo = false;
+				currentStep.setText("Step Number : "+stepNumber);
+				currentStep.setTextFill(Color.WHITE);
+			}
+			if(!paused || step || moveTo){
+				if(step){
+					step = false;
+				}
+				currentStep.setText("Step Number : "+stepNumber);
+				currentStep.setTextFill(Color.WHITE);
+				stepNumber++;
+				for(int i=0;i<size;i++){
+					for(int y=0;y<size;y++){
+						int nbAlive = 0;
+						int difx =0;
+						if(i>0){
+							if(i<size-1){
+								difx=1;
+							}
+							else{
+								difx=0;
+							}
+						}
+						else{
+							difx=0;
+						}
+						int dify=0;
+						if(y>0){
+							if(y<size-1){
+								dify=1;
+							}
+							else{
+								dify=0;
+							}
+						}
+						else{
+							dify=0;
+						}
+						if(lstPane[i-difx][y-dify].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i][y-dify].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i+difx][y-dify].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i-difx][y].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i+difx][y].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i-difx][y+dify].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i][y+dify].isAlive()){
+							nbAlive++;
+						}
+						if(lstPane[i+difx][y+dify].isAlive()){
+							nbAlive++;
+						}
+						if(nbAlive>3 || nbAlive<2){
+							alive.add(lstPane[i][y]);
+						}
+						else if(nbAlive==3){
+							toRez.add(lstPane[i][y]);
+						}
+					}
+				}
+				for(Cells inAlive : alive){
+					if(inAlive.isAlive()){
+						inAlive.setAlive(false);
+						gc.setFill(Color.BLACK);
+						gc.fillRect(inAlive.getCoordX(), inAlive.getCoordY(), scale, scale);
+					}
+				}
+				alive.clear();
+				for(Cells inToRez : toRez){
+					if(!inToRez.isAlive()){
+						inToRez.setAlive(true);
+						gc.setFill(Color.WHITE);
+						gc.fillRect(inToRez.getCoordX(), inToRez.getCoordY(), scale, scale);
+					}
+				}
+				toRez.clear();
+			}
+			long sec = System.currentTimeMillis();
+			if((sec - start) >= 1000 ){
+				actualFrameLabel.setText("Actual Frame : "+String.valueOf(frameCnt));
+				actualFrameLabel.setTextFill(Color.WHITE);
+				frameCnt = 0;
+				setStart(System.currentTimeMillis());
+			}
+			else{
+				frameCnt++;
+				if(frameCnt<getTargetFrame() && gameSpeedSecs>0){
+					gameSpeedSecs -= 0.00001;
+				}
+				else{
+					gameSpeedSecs += 0.00001;
+				}
+				loop.setUpdate((long)(gameSpeedSecs * 1000000000L));
+			}
+		});
 		scene.setOnKeyReleased(e->{
 			if(e.getCode().equals(KeyCode.ENTER)){
 				if(paused){
@@ -153,7 +270,8 @@ public class Frame extends Application{
 					for(int y=0;y<size;y++){
 						if(lstPane[i][y].isAlive()){
 							lstPane[i][y].setAlive(false);
-							lstPane[i][y].setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+							gc.setFill(Color.BLACK);
+							gc.fillRect(lstPane[i][y].getCoordX(), lstPane[i][y].getCoordY(), scale, scale);
 						}
 					}
 				}
@@ -163,7 +281,8 @@ public class Frame extends Application{
 					for(int y=0;y<size;y++){
 						if(!lstPane[i][y].isAlive()){
 							lstPane[i][y].setAlive(true);
-							lstPane[i][y].setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+							gc.setFill(Color.WHITE);
+							gc.fillRect(lstPane[i][y].getCoordX(), lstPane[i][y].getCoordY(), scale, scale);
 						}
 					}
 				}
@@ -174,10 +293,74 @@ public class Frame extends Application{
 						int rdm = 0 + (int)(Math.random() * ((10 - 0) + 1));
 						if(rdm == 10){
 							lstPane[i][y].setAlive(true);
-							lstPane[i][y].setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+							gc.setFill(Color.WHITE);
+							gc.fillRect(lstPane[i][y].getCoordX(), lstPane[i][y].getCoordY(), scale, scale);
+						}
+						else{
+							gc.setFill(Color.BLACK);
+							gc.fillRect(lstPane[i][y].getCoordX(), lstPane[i][y].getCoordY(), scale, scale);
 						}
 					}
 				}
+			}
+		});
+		scene.setOnKeyPressed(e->{
+			switch(e.getCode()){
+			case LEFT:{
+				translate(-100, 0, gc);
+				break;
+			}
+			case RIGHT:{
+				translate(100, 0, gc);
+				break;
+			}
+			case UP:{
+				translate(0, -100, gc);
+				break;
+			}
+			case DOWN:{
+				translate(0, 100, gc);
+				break;
+			}
+			default:{
+				break;
+			}
+			}
+		});
+		scene.setOnScroll(e->{
+			boolean wasPaused = false;
+			if(isPaused()){
+				wasPaused = true;
+			}
+			setPaused(true);
+			for(Cells inLstCells : lstCells){
+				gc.clearRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+			}
+			if(e.getDeltaY()>0){
+				scale++;
+			}
+			else{
+				scale--;
+			}
+			for(int i=0;i<size;i++){
+				for(int y=0;y<size;y++){
+					Cells cell = lstPane[i][y];
+					cell.setCoordX((i*scale)+i);
+					cell.setCoordY((y*scale)+y);
+				}
+			}
+			for(Cells inLstCells : lstCells){
+				if(inLstCells.isAlive()){
+					gc.setFill(Color.WHITE);
+					gc.fillRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+				}
+				else{
+					gc.setFill(Color.BLACK);
+					gc.fillRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+				}
+			}
+			if(!wasPaused){
+				setPaused(false);
 			}
 		});
 		stage.setScene(scene);
@@ -186,13 +369,98 @@ public class Frame extends Application{
 			Platform.exit();
 		});
 		stage.show();
+		loop.start();
+	}
+	public void translate(double x, double y, GraphicsContext gc){
+		boolean wasPaused = false;
+		if(isPaused()){
+			wasPaused = true;
+		}
+		setPaused(true);
+		for(Cells inLstCells : lstCells){
+			gc.clearRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+		}
+		gc.translate(x, y);
+		translatedX = translatedX + (int)x;
+		translatedY = translatedY +(int)y;
+		for(Cells inLstCells : lstCells){
+			if(inLstCells.isAlive()){
+				gc.setFill(Color.WHITE);
+				gc.fillRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+			}
+			else{
+				gc.setFill(Color.BLACK);
+				gc.fillRect(inLstCells.getCoordX(), inLstCells.getCoordY(), scale, scale);
+			}
+		}
+		if(!wasPaused){
+			setPaused(false);
+		}
+	}
+	public int getSize() {
+		return size;
+	}
 
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public Cells[][] getLstPane() {
+		return lstPane;
+	}
+
+	public void setLstPane(Cells[][] lstPane) {
+		this.lstPane = lstPane;
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
+	}
+
+	/**
+	 * @return the start
+	 */
+	public long getStart() {
+		return start;
+	}
+
+	/**
+	 * @param start the start to set
+	 */
+	public void setStart(long start) {
+		this.start = start;
+	}
+
+	public int getTargetFrame() {
+		return targetFrame;
+	}
+
+	public void setTargetFrame(int targetFrame) {
+		this.targetFrame = targetFrame;
 	}
 }
-class CustomPane extends Pane{
+class Cells{
 	private boolean alive;
-	public CustomPane(){
-		super();
+	private int CoordX;
+	private int CoordY;
+	public Cells(){
+		this.alive = true;
+	}
+	public Cells(int x, int y){
+		this.CoordX = x;
+		this.CoordY = y;
 		this.alive = true;
 	}
 	public boolean isAlive() {
@@ -200,5 +468,82 @@ class CustomPane extends Pane{
 	}
 	public void setAlive(boolean alive) {
 		this.alive = alive;
+	}
+	public int getCoordX() {
+		return CoordX;
+	}
+	public void setCoordX(int coordX) {
+		CoordX = coordX;
+	}
+	public int getCoordY() {
+		return CoordY;
+	}
+	public void setCoordY(int coordY) {
+		CoordY = coordY;
+	}
+}
+class Loop extends AnimationTimer{
+
+	private long lastTime = IDEALFRAMERATENS;
+	private Consumer<Long> doEveryUpdate;
+	private long updateGraphicsEvery;
+
+	public Loop(long updateEveryNS, Consumer<Long> f){
+		this.updateGraphicsEvery = updateEveryNS;
+		this.doEveryUpdate = f;
+	}
+
+	@Override
+	public void handle(long currentTime) {
+
+		long elapsedTime = currentTime - lastTime;
+		if(elapsedTime < updateGraphicsEvery){
+			return;
+		}
+		else{
+			lastTime = currentTime;
+			doEveryUpdate.accept(elapsedTime);
+		}
+	}
+	public final static long NANOSPERSECOND = 1000000000;
+	public final static long IDEALFRAMERATENS = (long)(1 / 60.0 * NANOSPERSECOND);
+	public void setUpdate(long update){
+		this.updateGraphicsEvery = update;
+	}
+
+}
+class Player{
+	private double x;
+	private double y;
+	public Player(){
+
+	}
+	public Player(double x,double y){
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	 * @return the x
+	 */
+	public double getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(double x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public double getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(double y) {
+		this.y = y;
 	}
 }
